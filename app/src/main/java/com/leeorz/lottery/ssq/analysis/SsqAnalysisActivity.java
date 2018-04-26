cd346030622d539c6cf15b39b8f6f2d9576cf000
leekajun <378229364@qq.com>
2018-04-26 10:42:14 +0800

    增加分析列表的界面

--- app/src/main/java/com/leeorz/lottery/ssq/analysis/SsqAnalysisActivity.java
+++ app/src/main/java/com/leeorz/lottery/ssq/analysis/SsqAnalysisActivity.java
@@ -1,11 +1,20 @@
 package com.leeorz.lottery.ssq.analysis;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.leeorz.lib.base.BaseActivity;
+import com.leeorz.lib.base.BaseAdapter;
 import com.leeorz.lottery.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -23,6 +32,9 @@ public class SsqAnalysisActivity extends BaseActivity {
     ImageView ivBack;
     @BindView(R.id.tv_title)
     TextView tvTitle;
+    @BindView(R.id.lv_content)
+    ListView lvContent;
+    private AnalysisListAdapter analysisListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,12 +44,68 @@ public class SsqAnalysisActivity extends BaseActivity {
         initView();
     }
 
-    private void initView(){
+    private void initView() {
         tvTitle.setText("专家分析");
+
+        List<String> analysisList = new ArrayList();
+        analysisList.add("");
+        analysisList.add("");
+        analysisList.add("");
+        analysisList.add("");
+        analysisList.add("");
+        analysisList.add("");
+        analysisList.add("");
+        analysisList.add("");
+        analysisListAdapter = new AnalysisListAdapter(getActivity());
+        analysisListAdapter.setData(analysisList);
+        lvContent.setAdapter(analysisListAdapter);
+        lvContent.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                SsqAnalysisDetailActivity.gotoThis(getActivity());
+            }
+        });
+
     }
 
     @OnClick(R.id.iv_back)
     public void onViewClicked() {
         finish();
     }
+
+    private class AnalysisListAdapter extends BaseAdapter<String> {
+
+        public AnalysisListAdapter(Context context) {
+            super(context);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            super.getView(position, convertView, parent);
+            ViewHolder viewHolder;
+            if (convertView == null) {
+                convertView = mInflater.inflate(R.layout.template_analysis_list_item, null);
+                viewHolder = new ViewHolder(convertView);
+                convertView.setTag(viewHolder);
+            } else {
+                viewHolder = (ViewHolder) convertView.getTag();
+            }
+            return convertView;
+        }
+
+
+    }
+    static class ViewHolder {
+        @BindView(R.id.tv_title)
+        TextView tvTitle;
+        @BindView(R.id.tv_desc)
+        TextView tvDesc;
+        @BindView(R.id.tv_date)
+        TextView tvDate;
+
+        ViewHolder(View view) {
+            ButterKnife.bind(this, view);
+        }
+    }
+
 }
--- app/src/main/java/com/leeorz/lottery/ssq/analysis/SsqAnalysisDetailActivity.java
+++ app/src/main/java/com/leeorz/lottery/ssq/analysis/SsqAnalysisDetailActivity.java
@@ -1,6 +1,16 @@
 package com.leeorz.lottery.ssq.analysis;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
 import com.leeorz.lib.base.BaseActivity;
+import com.leeorz.lottery.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * author: leeorz
@@ -9,4 +19,28 @@ import com.leeorz.lib.base.BaseActivity;
  * description:
  */
 public class SsqAnalysisDetailActivity extends BaseActivity {
+
+    @BindView(R.id.tv_title)
+    TextView tvTitle;
+
+    public static void gotoThis(Context context) {
+        Intent intent = new Intent(context, SsqAnalysisDetailActivity.class);
+        context.startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ssq_analysis_detail);
+        ButterKnife.bind(this);
+        initView();
+    }
+    private void initView(){
+        tvTitle.setText("预测详情");
+    }
+
+    @OnClick(R.id.iv_back)
+    public void onViewClicked() {
+        finish();
+    }
 }
